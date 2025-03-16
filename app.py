98b7c29e5a5d7525057e6e4b34a00052311e5919
upathak1981 <95219280+upathak1981@users.noreply.github.com>
2025-03-17 10:49:06 +1100

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def mytemplate():
    strTemplate = strTemplate + "</form>"
    #Finsih Create a template finished
     
-   # Create a DB Connection with postgres db
+   # Create a DB Connection with postgres db in Heroku
    db_name = os.getenv('db_name')
    user_name = os.getenv('user_name')
    port = os.getenv('port')
